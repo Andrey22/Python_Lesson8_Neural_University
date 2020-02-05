1fa05c92ae0b3b3b996688eeaef4a44286aa4c7f
Andrey22 <aandreyko@mail.ru>
2020-02-06 02:14:22 +0300

    commit8

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ def estimatedtime (f):
     def wrapper(*args, **kwargs):
         startime=time.time()
         f(*args, **kwargs)
+        print(type(f))
         deltatime=(time.time() - startime)
-        return print ('Время выполнения создания списка', deltatime )
+        return print ('Время выполнения создания списка', format(deltatime, '11') )
     return wrapper
 
 @estimatedtime
@@ -17,10 +18,12 @@ def makelist(n):
     list=[]
     for i in range(1, n+1):
         list.append(i)
+    print(type(list))
 makelist(1000000)
 
 @estimatedtime
 def makelistgen(n):
-    list=[i for i in range(1,n+1)]
-makelistgen(1000000)
+    list=(i for i in range(1,n+1))
+    print(type(list))
+makelistgen(1000000000000000000000000000000000000)
 # print(deltatime)
